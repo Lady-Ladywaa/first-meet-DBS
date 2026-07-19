a573a80c763cfef3cfa3d712882447433a0d514f
Lady-Ladywaa <kusuma.pkmn@gmail.com>
2026-07-19 12:48:11 +0700

    add new schema

--- 03_my-ecommerce-project/orders.mongodb.js
+++ 03_my-ecommerce-project/orders.mongodb.js
@@ -6,21 +6,42 @@ db.orders.deleteMany({});
 //Insert mock data for orders collection
 db.orders.insertMany([
     {
-      "order_id": ObjectId("..."),
-      "user_id": ObjectId("6a5439e71d3bd7ec6e9a69e0"),
+      "orderid": ObjectId("..."),
+      "userid": ObjectId("6a5439e71d3bd7ec6e9a69e0"),
       "products": [
         {
-            "product_id": ObjectId("6a5439e751dda74c52c60170"),
-            "product_name": "angel dress",
+            "productid": ObjectId("6a5439e751dda74c52c60170"),
+            "productname": "angel dress",
             "quantity": 1,
             "size": "M",
             "color": "blue",
             "price": 1500,
-            "total price": "1500"
+            "totalprice": 1500
 
+        },
+        {"productid":  ObjectId(".."),
+          "productname": "hijab ปักมุก",
+          "quantity": 1,
+          "color": "blue",
+          "price": 390,
+          "totalprice": 390
         }
-      ]
+      ],
+       
+
+    totalAmount: 1890,
+
+    paymentStatus: "Pending",
+
+    orderStatus: "Pending",
+
+    createdAt: new Date(),
+
+    updatedAt: new Date()
+      
 
     }
+
+    
 ]);
 db.orders.find()
---
+++ 03_my-ecommerce-project/rentalBookings.mongodb.js
@@ -0,0 +1,32 @@
+use("Wa-Vibe-Fashion-db")
+
+//Clear existing data
+db.rentalBookings.deleteMany({});
+
+//Insert mock data for rentalbookings collection
+db.rentalBookings.insertMany([
+    {
+        "userId": ObjectId(""),
+        "items":
+        [
+            {
+               "productId": ObjectId(".."),
+               "productName": "wawa dress",
+               "rentalPrice": 1000,  
+               "deposit": 500,
+            }
+        ],
+        
+        "totalAmount": 1500,                 // เงินที่ลุกค้าต้องจ่ายในตอนแรก คือค่าเช่าชุด/เครื่องประดับ+เงินมัดจำ
+        "rentalStartDate": new Date(""),  //วันที่ลูกค้ารับชุดเช่า
+        "rentalStatus": "Reserved",     //สถานะการเช่า  Reserved=จองแล้ว Borrowed=กำลังเช่าอยู่ Returned=คืนแล้ว Completed=เสร็จสมบูรณ์คือ ตรวจสภาพแล้ว คืนชุดและเงินมัดจำแล้ว
+        "returnStatus": "Waiting",       //สถานะการคืนสินค้า  Waiting= คือลูกค้ายังไม่คืนสินค้า  Returned= ลูกค้าคืนสินค้าแล้วแต่ว่ายังต้องตรวจสอบก่อน Checking=กำลังตรวจสอบ Damaged=ชุดเสียหาย
+        "rentalEndDate": new Date(""),   // วันครบกำหนดลูกค้าคืนชุด
+        "actualReturnDate":  null,       // วันที่ลูกค้านำมาคืนจริง  ถ้าลูกค้าคืนจริงแล้วค่อยใส่วันที่ new Date(""),         
+        "paymentStatus": "Pending",           // paymentStatus คือ สถานะการชำระเงิน  Pending=รอชำระ Paid=ชำระเงินแล้ว Refunded=คืนเงินแล้ว เช่นลูกค้ายกเลิก, ร้านยกเลิก      
+        "createdAt": new Date(),
+        "updatedAt": new Date()
+
+    }
+
+])
